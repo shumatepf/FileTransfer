a54baf6f7adb9a550e36db7b9f8c2259e995ff26
Peter Shumate <shumatpf@dukes.jmu.edu>
2018-08-31 14:59:29 -0400

    update directories for current semester

--- mydirectories.h
+++ mydirectories.h
@@ -2,16 +2,16 @@
 #define CONSTANT_FILE
 
 char *SRC_PATH = "/Users/Peter/Documents/School/SRC/";
-char *C1 = "-CS261";
-char *C1_DIR = "/Users/Peter/Documents/School/Spring 2018/CS 261/";
-char *C2 = "-CS260";
-char *C2_DIR = "/Users/Peter/Documents/School/Spring 2018/CS 260/";
-char *C3 = "-PSYC101";
-char *C3_DIR = "/Users/Peter/Documents/School/Spring 2018/Psych 101/";
-char *C4 = "-RUS320";
-char *C4_DIR = "/Users/Peter/Documents/School/Spring 2018/Russian/";
-char *C5 = "-MUS200";
-char *C5_DIR = "/Users/Peter/Documents/School/Spring 2018/Music/";
+char *C1 = "-CS327";
+char *C1_DIR = "/Users/Peter/Documents/School/Fall 2018/CS327/";
+char *C2 = "-CS361";
+char *C2_DIR = "/Users/Peter/Documents/School/Fall 2018/CS361/";
+char *C3 = "-GEOL211";
+char *C3_DIR = "/Users/Peter/Documents/School/Fall 2018/GEOL211/";
+char *C4 = "-ISAT252";
+char *C4_DIR = "/Users/Peter/Documents/School/Fall 2018/ISAT252/";
+char *C5 = "-MATH220";
+char *C5_DIR = "/Users/Peter/Documents/School/Fall 2018/MATH220/";
 
 #endif
 
