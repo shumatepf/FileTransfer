f607f194bb9937d18b7434599bf65acf28052ef4
shumatpf <shumatpf@dukes.jmu.edu>
2019-02-15 13:08:59 -0500

    update directories for current semester

--- mydirectories.h
+++ mydirectories.h
@@ -1,17 +1,15 @@
 #ifndef CONSTANT_FILE
 #define CONSTANT_FILE
 
-char *SRC_PATH = "/Users/Peter/Documents/School/SRC/";
-char *C1 = "-CS327";
-char *C1_DIR = "/Users/Peter/Documents/School/Fall 2018/CS327/";
-char *C2 = "-CS361";
-char *C2_DIR = "/Users/Peter/Documents/School/Fall 2018/CS361/";
-char *C3 = "-GEOL211";
-char *C3_DIR = "/Users/Peter/Documents/School/Fall 2018/GEOL211/";
-char *C4 = "-ISAT252";
-char *C4_DIR = "/Users/Peter/Documents/School/Fall 2018/ISAT252/";
-char *C5 = "-MATH220";
-char *C5_DIR = "/Users/Peter/Documents/School/Fall 2018/MATH220/";
+char *SRC_PATH = "~/Documents/school/SRC";
+char *C1 = "-CS430";
+char *C1_DIR = "~/school/spr19/cs430/";
+char *C2 = "-CS482";
+char *C2_DIR = "~/school/spr19/cs482/";
+char *C3 = "-GEOL110";
+char *C3_DIR = "~/school/spr19/geol110/";
+char *C5 = "-ENG221";
+char *C5_DIR = "~/school/spr19/eng221/";
 
 #endif
 
